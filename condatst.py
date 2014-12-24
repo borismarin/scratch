32ac46a9ef16551cd558d96d5ea19b7ed6abbc61
Boris Marin <borismarin@gmail.com>
2014-12-24 13:47:55 +0000

    Update condatst.py

--- condatst.py
+++ condatst.py
@@ -6,7 +6,7 @@ import conda_api
 engine = os.environ.get('OMV_ENGINE')
 if 'genesis' in engine:
     conda_api.set_root_prefix()
-    conda_api.install('hdf5')
+    conda_api.install(('hdf5'))
     
 
   
